6a1a666479c57872bd4d85aad5442f2c9a4d39c6
Sanjar Suyunov <sanjarsuyunov1304@gmail.com>
2023-06-06 16:20:54 +0500

    Configured mysql connnection

--- connection.js
+++ connection.js
@@ -1,1 +1,15 @@
-const mysql = require('mysql')
+const mysql = require('mysql');
+require('dotenv').config();
+
+var connection = mysql.createConnection({
+    port: process.env.DB_PORT,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
+
+connection.connect((err) => {
+    !err ? console.log('connected') : console.log(err)
+});
+module.exports = connection;
