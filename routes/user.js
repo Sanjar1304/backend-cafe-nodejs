24acf9475c5e53f5583ae5e496bd160c9834941c
Sanjar Suyunov <sanjarsuyunov1304@gmail.com>
2023-06-07 14:20:56 +0500

    Added access token and login api

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const connection = require('../connection');
-const router = express.Router()
+const router = express.Router();
+
+const jwt = require('jsonwebtoken');
+require('dotenv').config()
 
 router.post('/signup', (req, res) => {
     let user = req.body;
@@ -20,11 +23,32 @@ router.post('/signup', (req, res) => {
                 return res.status(400).json({ message: 'Email already exists.' });
             }
         } else {
-            return res.status(500).json(err)
+            return res.status(500).json(err);
         }
     });
 })
 
 
+router.post('/login', (req, res) => {
+    const user = req.body;
+    query = 'select email,password,role,status from user where email=?';
+    connection.query(query, [user.email], (err, results) => {
+        if (!err) {
+            if (results.length <= 0 || results[0].password != user.password) {
+                return res.status(401).json({ message: 'Incorrect Username or Password' })
+            } else if (results[0].status === 'false') {
+                return res.status(401).json({ message: 'Wait for Admin Approval' })
+            } else if (results[0].password == user.password) {
+                const response = { email: results[0].email, role: results[0].role };
+                const accessToken = jwt.sign(response, process.env.ACCESS_TOKEN, { expiresIn: '8h' });
+                res.status(200).json({ token: accessToken });
+            } else {
+                return res.status(400).json({ message: 'Something went wrong. Please try again later' })
+            }
+        } else {
+            return res.status(500).json(err);
+        }
+    })
+})
 
 module.exports = router
