bf279fea8c2dac1eed1e01ff2f555220f355aa86
Sanjar Suyunov <sanjarsuyunov1304@gmail.com>
2023-06-12 11:22:20 +0500

    Added category sql query

--- table.sql
+++ table.sql
@@ -10,3 +10,10 @@ create table user(
 );
 
 insert into user(name,contactNumber,email,password,status,role) values('Admin','123456789','admin@gmail.ru','admin','true','admin');
+
+
+create table category(
+    id int NOT NULL AUTO_INCREMENT,
+    name varchar(255) NOT NULL,
+    primary key(id) 
+)
