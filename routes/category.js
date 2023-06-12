4bba100c6787eafcff5c3c545137b6dfb0f68097
Sanjar Suyunov <sanjarsuyunov1304@gmail.com>
2023-06-12 12:21:03 +0500

    Refactored if conditions

--- routes/category.js
+++ routes/category.js
@@ -12,11 +12,7 @@ router.post('/add', auth.authenticateToken, checkRole.checkRole, (req, res, next
     let category = req.body;
     query = 'insert into category (name) values(?)';
     connection.query(query, [category.name], (err, results) => {
-        if (!err) {
-            return res.status(200).json({ message: 'New category added successfully.' })
-        } else {
-            return res.status(500).json(err)
-        }
+        !err ? res.status(200).json({ message: 'New category added successfully.' }) : res.status(500).json(err)
     })
 });
 
@@ -25,10 +21,6 @@ router.post('/add', auth.authenticateToken, checkRole.checkRole, (req, res, next
 router.get('/get', auth.authenticateToken, (req, res, next) => {
     query = 'select * from category order by name';
     connection.query(query, (err, results) => {
-        if (!err) {
-            return res.status(200).json(results);
-        } else {
-            return res.status(500).json(err)
-        }
+        !err ? res.status(200).json(results) : res.status(500).json(err)
     })
 })
