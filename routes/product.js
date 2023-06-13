fb0d403e43622c4a93186ad10aef1103049518e8
Sanjar Suyunov <sanjarsuyunov1304@gmail.com>
2023-06-13 11:42:24 +0500

    Added addProduct api

--- routes/product.js
+++ routes/product.js
@@ -3,3 +3,13 @@ const connection = require('../connection');
 const router = express.Router();
 var auth = require('../services/authentication');
 var checkRole = require('../services/checkRole');
+
+
+// Add product API
+router.post('/add', auth.authenticateToken, checkRole.checkRole, (req, res) => {
+    let product = req.body;
+    let query = "insert into product (name,categoryId,description,price,status) values(?,?,?,?,'true')";
+    connection.query(query, [product.name, product.categoryId, product.description, product.price], (err, results) => {
+        !err ? res.status(200).json({ message: "Product added successfully." }) : res.status(500).json(err);
+    })
+});
