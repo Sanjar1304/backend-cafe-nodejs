6352ccc7daa7a9c1bf58b5b06def554744ad2632
Sanjar Suyunov <sanjarsuyunov1304@gmail.com>
2023-06-13 14:14:39 +0500

    Added getAllProducts api

--- routes/product.js
+++ routes/product.js
@@ -8,8 +8,19 @@ var checkRole = require('../services/checkRole');
 // Add product API
 router.post('/add', auth.authenticateToken, checkRole.checkRole, (req, res) => {
     let product = req.body;
-    let query = "insert into product (name,categoryId,description,price,status) values(?,?,?,?,'true')";
+    var query = "insert into product (name,categoryId,description,price,status) values(?,?,?,?,'true')";
     connection.query(query, [product.name, product.categoryId, product.description, product.price], (err, results) => {
         !err ? res.status(200).json({ message: "Product added successfully." }) : res.status(500).json(err);
     })
 });
+
+
+// Get all products API
+router.get('/get', auth.authenticateToken, (req, res, next) => {
+    var query = "select p.id,p.name,p.description,p.price,p.status,c.id as categoryId,c.name as categoryName from product as p INNER JOIN category as c where p.categoryId = c.id";
+    connection.query(query, (err, results) => {
+        !err ? res.status(200).json(results) : res.status(500).json(err);
+    })
+})
+
+module.exports = router;
