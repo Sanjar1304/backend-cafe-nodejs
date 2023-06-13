303df67167019e3f03297595f52e048159c373fb
Sanjar Suyunov <sanjarsuyunov1304@gmail.com>
2023-06-13 15:05:27 +0500

    Added update product api

--- routes/product.js
+++ routes/product.js
@@ -34,12 +34,29 @@ router.get('/getByCategory/:id', auth.authenticateToken, (req, res, next) => {
 });
 
 
-// 
+// Get By Id products API
 router.get('/getById/:id', auth.authenticateToken, (req, res, next) => {
     const id = req.params.id;
     var query = "select id,name,description,price from product where id=?";
     connection.query(query, [id], (err, results) => {
         !err ? res.status(200).json(results[0]) : res.status(500).json(err);
+    })
+});
+
+
+// Update product API
+router.patch('/update', auth.authenticateToken, checkRole.checkRole, (req, res, next) => {
+    let product = req.body;
+    var query = "update product set name=?,categoryId=?,description=?,price=? where id=?";
+    connection.query(query, [product.name, product.categoryId, product.description, product.price, product.id], (err, results) => {
+        if (!err) {
+            if (results.affectedRows == 0) {
+                return res.status(404).json({ message: "Product id does not exist." });
+            }
+            return res.status(200).json({ message: "Product updated successfully." });
+        } else {
+            return res.status(500).json(err);
+        }
     })
 });
 
