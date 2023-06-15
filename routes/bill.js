79c8afec6c221081642c0a86f17f49da931362d4
Sanjar Suyunov <sanjarsuyunov1304@gmail.com>
2023-06-15 12:11:01 +0500

    Added pdf generating API

--- routes/bill.js
+++ routes/bill.js
@@ -1,5 +1,55 @@
 const express = require('express');
 const connection = require('../connection');
 const router = express.Router();
+let ejs = require('ejs');
+let pdf = require('html-pdf');
+let path = require('path');
+var fs = require('fs');
+var uuid = require('uuid');
 var auth = require('../services/authentication');
-var checkRole = require('../services/checkRole');
+
+
+// generateReport API
+router.post('/generateReport', auth.authenticateToken, (req, res) => {
+    const generatedUUID = uuid.v1();
+    const orderDetails = req.body;
+    var productDetailsReport = JSON.parse(orderDetails.productDetails);
+
+    var query = "insert into bill (name,uuid,email,contactNumber,paymentMethod,total,productDetails,createdBy) values(?,?,?,?,?,?,?,?)";
+    connection.query(query, [
+        orderDetails.name,
+        generatedUUID,
+        orderDetails.email,
+        orderDetails.contactNumber,
+        orderDetails.paymentMethod,
+        orderDetails.totalAmount,
+        orderDetails.productDetails,
+        res.locals.email
+    ], (err, results) => {
+        if (!err) {
+            ejs.renderFile(path.join(__dirname, '', "report.ejs"), {
+                productDetails: productDetailsReport,
+                name: orderDetails.name,
+                email: orderDetails.email,
+                contactNumber: orderDetails.contactNumber,
+                paymentMethod: orderDetails.paymentMethod,
+                totalAmount: orderDetails.totalAmount
+            }, (err, results) => {
+                if (err) {
+                    return res.status(500).json(err)
+                } else {
+                    pdf.create(data).toFile('./generated_pdf/' + generatedUUID + '.pdf', function(err, data) {
+                        if (err) {
+                            console.log(err);
+                            return res.status(500).json(err);
+                        } else {
+                            return res.status(200).json({ uuid: generatedUUID });
+                        }
+                    })
+                }
+            })
+        } else {
+            return res.status(500).json(err);
+        }
+    })
+});
