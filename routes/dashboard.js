3ea5d1f1b6a7abc7a0bb5b943df71899a3e15150
Sanjar Suyunov <sanjarsuyunov1304@gmail.com>
2023-06-19 12:34:21 +0500

    Finished getDashboardDetails api

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -5,9 +5,36 @@ var auth = require('../services/authentication');
 var checkRole = require('../services/checkRole');
 
 
-// get Details API
+// get Dashboard Details API
 router.get('/details', auth.authenticateToken, (req, res, next) => {
+    var categoryCount;
+    var productCount;
+    var billCount;
 
+    var query = "select count(id) as categoryCount from category";
+    connection.query(query, (err, results) => {
+        !err ? categoryCount = results[0].categoryCount : res.status(500).json(err);
+    });
+
+    var query = "select count(id) as productCount from product";
+    connection.query(query, (err, results) => {
+        !err ? productCount = results[0].productCount : res.status(500).json(err);
+    });
+
+    var query = "select count(id) as billCount from bill";
+    connection.query(query, (err, results) => {
+        if (!err) {
+            billCount = results[0].billCount;
+            var data = {
+                category: categoryCount,
+                product: productCount,
+                bill: billCount
+            };
+            return res.status(200).json(data);
+        } else {
+            res.status(500).json(err);
+        }
+    })
 });
 
 module.exports = router;
