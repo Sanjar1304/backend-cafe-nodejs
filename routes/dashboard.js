44f177b74415ab12e77690dc9945c76c5d2bb9b7
Sanjar Suyunov <sanjarsuyunov1304@gmail.com>
2023-06-20 11:51:48 +0500

    Deleted unnessary variable in dashboard module

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -2,7 +2,6 @@ const express = require('express');
 const connection = require('../connection');
 const router = express.Router();
 var auth = require('../services/authentication');
-var checkRole = require('../services/checkRole');
 
 
 // get Dashboard Details API
